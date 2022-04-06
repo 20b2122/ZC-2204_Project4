b6641366635885c7863ac62d69ab5d1d67d1c8ef
Nadeeya <78889312+Nadeeya@users.noreply.github.com>
2022-04-07 06:50:13 +0800

    Add files via upload

---
+++ retrieveApplicant.php
@@ -0,0 +1,18 @@
+<?php
+include ('database.php');
+
+$jobName = $_GET['jobName'];
+$IC = $_GET['IC'];
+
+$sql = "UPDATE applicant SET Applicant_status= 'PENDING' WHERE IC='".$IC."' AND jobName='".$jobName."'";
+if (mysqli_query($conn, $sql)) {
+} else {
+  echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+header("Refresh:0; url=viewApplicants.php?jobname=".$jobName);
+
+
+?>
