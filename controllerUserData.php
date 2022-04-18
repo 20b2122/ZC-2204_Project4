81907f517e425edf9afd7290d46a5906cb1cd0f0
Farah20b2076 <99034556+Farah20b2076@users.noreply.github.com>
2022-04-19 05:56:10 +0800

    Add files via upload

---
+++ controllerUserData.php
@@ -0,0 +1,94 @@
+<?php 
+session_start();
+require "database.php";
+$email = "";
+$name = "";
+$errors = array();
+
+    //if user click login button
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $check_email = "SELECT * FROM recruiter WHERE Email = '$email'";
+        $res = mysqli_query($conn, $check_email);
+        if(mysqli_num_rows($res) > 0){
+            $fetch = mysqli_fetch_assoc($res);
+            $fetch_pass = $fetch['password'];
+            if(password_verify($password, $fetch_pass)){
+                $_SESSION['email'] = $email;
+                $status = $fetch['status'];
+                if($status == 'verified'){
+                  $_SESSION['email'] = $email;
+                  $_SESSION['password'] = $password;
+                    header('location: mainRecruiterPage.php');
+                }else{
+                    $info = "It's look like you haven't still verify your email - $email";
+                    $_SESSION['info'] = $info;
+                    header('location: login.php');
+                }
+            }else{
+                $errors['email'] = "Incorrect email or password!";
+            }
+        }else{
+            $errors['email'] = "It's look like you're not yet a member! Click on the bottom link to signup.";
+        }
+    }
+
+    //if user click continue button in forgot password form
+    if(isset($_POST['check-email'])){
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $check_email = "SELECT * FROM recruiter WHERE email='$email'";
+        $run_sql = mysqli_query($conn, $check_email);
+        if(mysqli_num_rows($run_sql) > 0){
+            $code = rand(999999, 111111);
+            $insert_code = "UPDATE recruiter SET code = $code WHERE email = '$email'";
+            $run_query =  mysqli_query($conn, $insert_code);
+            if($run_query){
+                $subject = "Password Reset Code";
+                $message = "Your password reset code is $code";
+                $sender = "From: itwasmiya@gmail.com";
+                if(mail($email, $subject, $message, $sender)){
+                    $info = "We've sent a password reset otp to your email - $email";
+                    $_SESSION['info'] = $info;
+                    $_SESSION['email'] = $email;
+                    header('location: reset-code.php');
+                    exit();
+                }else{
+                    $errors['otp-error'] = "Failed while sending code!";
+                }
+            }else{
+                $errors['db-error'] = "Something went wrong!";
+            }
+        }else{
+            $errors['email'] = "This email address does not exist!";
+        }
+    }
+
+    //if user click change password button
+    if(isset($_POST['change-password'])){
+        $_SESSION['info'] = "";
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+        $cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);
+        if($password !== $cpassword){
+            $errors['password'] = "Confirm password not matched!";
+        }else{
+            $code = 0;
+            $email = $_SESSION['email']; //getting this email using session
+            $encpass = password_hash($password, PASSWORD_BCRYPT);
+            $update_pass = "UPDATE recruiter SET code = $code, password = '$encpass' WHERE email = '$email'";
+            $run_query = mysqli_query($con, $update_pass);
+            if($run_query){
+                $info = "Your password changed. Now you can login with your new password.";
+                $_SESSION['info'] = $info;
+                header('Location: password-changed.php');
+            }else{
+                $errors['db-error'] = "Failed to change your password!";
+            }
+        }
+    }
+    
+   //if login now button click
+    if(isset($_POST['login-now'])){
+        header('Location: login.php');
+    }
+?>
